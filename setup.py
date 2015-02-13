7815c86e313df10849ba682fa79b5a4ccd69daba
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2015-02-13 03:10:10 +0000

    add installation procedure

---
+++ setup.py
@@ -0,0 +1,21 @@
+#+
+# Distutils script to install python_freetype. Invoke from the command line
+# in this directory as follows:
+#
+#     python3 setup.py install
+#
+# Written by Lawrence D'Oliveiro <ldo@geek-central.gen.nz>.
+#-
+
+import distutils.core
+
+distutils.core.setup \
+  (
+    name = "Python FreeType",
+    version = "0.4",
+    description = "anguage bindings for the FreeType library, for Python 3.2 or later",
+    author = "Lawrence D'Oliveiro",
+    author_email = "ldo@geek-central.gen.nz",
+    url = "http://github.com/ldo/python_freetype",
+    py_modules = ["freetype"],
+  )
