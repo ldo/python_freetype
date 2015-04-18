b128637f9d9264261f49e2bad2d09fa9e9a9a8e0
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2015-04-18 23:50:13 +0000

    Unfortunately, this stopped help(module) from showing module contents

--- freetype2.py
+++ freetype2.py
@@ -31,8 +31,6 @@ interface to Pycairo, if installed:
 # or later, to be compatible with FreeType itself.
 #-
 
-__all__ = ("death_to_wildcard_imports",)
-
 import math
 from numbers import \
     Number
