0c30a4a8821eaef756395f3b5d3a6c337ac67f5e
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2018-05-04 00:31:12 +0000

    add config for uploading to PyPI

--- setup.py
+++ setup.py
@@ -13,9 +13,11 @@ distutils.core.setup \
   (
     name = "Python FreeType",
     version = "0.6",
-    description = "language bindings for the FreeType library, for Python 3.2 or later",
+    description = "language bindings for FreeType",
+    long_description = "language bindings for the FreeType library, for Python 3.2 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
     url = "http://github.com/ldo/python_freetype",
+    license = "FTL/LGPL v2.1+",
     py_modules = ["freetype2"],
   )
