2b24ee148dbfb3fce649147aa085ee92130ae465
Lawrence D'Oliveiro <ldo@geek-central.gen.nz>
2019-09-16 06:51:22 +0000

    change URL to HTTPS

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ distutils.core.setup \
     long_description = "language bindings for the FreeType library, for Python 3.2 or later",
     author = "Lawrence D'Oliveiro",
     author_email = "ldo@geek-central.gen.nz",
-    url = "http://github.com/ldo/python_freetype",
+    url = "https://github.com/ldo/python_freetype",
     license = "FTL/LGPL v2.1+",
     py_modules = ["freetype2"],
   )
